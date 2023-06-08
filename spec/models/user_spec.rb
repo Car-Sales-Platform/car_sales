6dfc184cdc441f996c1229b8a72e3507132aae1a
Joshua Waiswa <jwaiswa7@gmail.com>
2023-06-08 15:35:07 +0300

    Used described class

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,36 +2,36 @@
 
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe described_class, type: :model do
   describe 'validations' do
     it 'requires phone number, first name, and last name' do
-      user = User.new
-      expect(user).not_to be_valid
-      expect(user.errors[:phone_number]).to include("can't be blank")
-      expect(user.errors[:first_name]).to include("can't be blank")
-      expect(user.errors[:last_name]).to include("can't be blank")
+      described_class = described_class.new
+      expect(described_class).not_to be_valid
+      expect(described_class.errors[:phone_number]).to include("can't be blank")
+      expect(described_class.errors[:first_name]).to include("can't be blank")
+      expect(described_class.errors[:last_name]).to include("can't be blank")
     end
   end
 
   describe 'devise modules' do
     it 'includes database_authenticatable' do
-      expect(User.devise_modules).to include(:database_authenticatable)
+      expect(described_class.devise_modules).to include(:database_authenticatable)
     end
 
     it 'includes registerable' do
-      expect(User.devise_modules).to include(:registerable)
+      expect(described_class.devise_modules).to include(:registerable)
     end
 
     it 'includes recoverable' do
-      expect(User.devise_modules).to include(:recoverable)
+      expect(described_class.devise_modules).to include(:recoverable)
     end
 
     it 'includes rememberable' do
-      expect(User.devise_modules).to include(:rememberable)
+      expect(described_class.devise_modules).to include(:rememberable)
     end
 
     it 'includes validatable' do
-      expect(User.devise_modules).to include(:validatable)
+      expect(described_class.devise_modules).to include(:validatable)
     end
   end
 end
