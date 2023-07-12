e04d22d4d8cbfdb38471456b91e42b9203b3f15c
stephenkati <stephenkatuli88@gmail.com>
2023-07-12 10:19:08 +0300

    add form validation

---
+++ app/javascript/controllers/search_controller.js
@@ -0,0 +1,35 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "form" ]
+
+  formSubmit(event) {
+    const form = event.target;
+    let isValid = this.validateForm(form);
+
+    if (!isValid) {
+      event.preventDefault();
+    }
+    console.log(isValid);
+  }
+
+  validateForm(form) {
+    let isValid = true;
+    let emptyFields = [];
+
+    let requiredFields = form.querySelectorAll('select');
+
+    requiredFields.forEach((field) => {
+      if (!field.disabled && !field.value.trim()) {
+        emptyFields.push(field);
+        isValid = false;
+      }
+    });
+
+    emptyFields.forEach((field) => {
+      field.focus();
+    });
+
+    return isValid;
+  }
+}
